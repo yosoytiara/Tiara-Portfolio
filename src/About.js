3c648f5d7e53473e5d7ceddfa3f3f084a9b7f00b
yosoytiara <114452621+yosoytiara@users.noreply.github.com>
2023-04-23 21:31:38 -0400

    edited text

--- src/About.js
+++ src/About.js
@@ -12,10 +12,10 @@ export default function About() {
             Hi, I am Tiara Dillard a Front-end Developer and Graphic Designer
             based in New York City. I was first introduced to Software
             engineering in highschool, and have been coding ever since. I am a
-            2020 Communications graduate from the University at Albany.from the
-            University at Albany. I have a passion for coding, and all things
-            art and fashion. I consider myself a creative,problem-solver and
-            someone who is always eager to learn new skills.
+            2020 Communications graduate from the University at Albany. I have a
+            passion for coding, and all things art and fashion. I consider
+            myself a creative,problem-solver and someone who is always eager to
+            learn new skills.
           </p>
         </div>
 
